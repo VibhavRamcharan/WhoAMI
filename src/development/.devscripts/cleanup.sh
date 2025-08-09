7102a17d8d09018ecd16fbcb28140fb328d52241
Vibhav Ramcharan <vibhav@maxcore.co.za>
2025-08-09 21:44:12 +0200

    feat(helpers): added dev scripts to clean-up repo

--- src/development/.devscripts/cleanup.sh
+++ src/development/.devscripts/cleanup.sh
@@ -5,5 +5,6 @@ echo "Cleaning up development folder..."
 find src/development -type d -name "bin" -exec rm -rf {} +
 find src/development -type d -name "obj" -exec rm -rf {} +
 find src/development -type d -name ".vs" -exec rm -rf {} +
+find src/development -type d -name "node_modules" -exec rm -rf {} +
 
 echo "Cleanup complete."
